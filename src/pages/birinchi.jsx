23339585a2bb2cf54c5ae8de7188420172f4285e
unknown <Fayzullohrahmatullayev245@gmail.comgit branch -M main>
2023-11-02 11:51:32 +0500

    edu on

---
+++ src/pages/birinchi.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import'../css/birinchi.css'
+export default function birinchi() {
+  return (
+    <div className='qq1'>
+      <nav className='qq2'>
+
+        <div className='qq3'>
+          
+          
+        </div>
+
+
+
+        <div className='qq4'>
+         
+
+        </div >
+
+        <div className='et4'>
+          <h1>reht </h1>
+
+        </div>
+
+
+
+
+      </nav>
+        
+    </div>
+  )
+}
